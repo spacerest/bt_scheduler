bd974954f45e8fdf0205b85eae3ada319e5b8852
spacerest <robconvo@gmail.com>
2017-05-09 20:58:48 -0500

    test

--- bt_scheduler/urls.py
+++ bt_scheduler/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
-from bt_scheduler.views import home, current_datetime, hours_ahead, contact, schedule
+from bt_scheduler.views import home, current_datetime, hours_ahead, contact, schedule, display_meta, thankyou
+
+from books import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^here/', admin.site.urls),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     url(r'^$', home),
     url(r'^contact/$', contact),
-    url(r'^schedule/$', schedule)
+    url(r'^schedule/$', schedule),
+    url(r'^display_meta/$', display_meta),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
+    url(r'^contact/thank-you/$', thankyou),
 ]
